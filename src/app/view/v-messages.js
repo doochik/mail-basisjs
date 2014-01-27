971d753b9312b4d6dd9e655d468147df68a467fe
Aleksei Androsov <aandrosov@yandex-team.ru>
2014-01-27 18:23:45 +0400

    activate vMessages

--- src/app/view/v-messages.js
+++ src/app/view/v-messages.js
@@ -1,9 +1,10 @@
 basis.require('basis.ui');
 
 var vMessages = new basis.ui.Node({
+    active: true,
     template: resource('../template/messages.tmpl'),
     binding: {
-//        mMessages: resource('../model/m-messages.js')
+        mMessages: resource('../model/m-messages.js')
     }
 });
 
